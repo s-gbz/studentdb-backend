5f5730479c40ea684baa77ed3b377e1aa9e97705
Marcell Dechant <marcell.dechant@neuefische.de>
2023-06-28 16:24:00 +0200

    BIG BANG

---
+++ src/main/java/de/neuefische/studentdbbackend/StudentdbBackendApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.studentdbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentdbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentdbBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/studentdbbackend/StudentdbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.studentdbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
